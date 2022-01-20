ad873c0c0fe09f23dae382b4b6d6206e7130e9c4
kanghyun98 <lkh981107@gmail.com>
2022-01-21 02:23:19 +0900

    update algorithm 22/01/20

---
+++ CS/Algorithm/programmers/level02/09 - 행렬 테두리 회전하기.js
@@ -0,0 +1,44 @@
+function solution(rows, columns, queries) {
+  const answer = [];
+  const arr = [];
+  for (let i = 1; i <= rows; i++) {
+    arr.push([...Array(columns)].map((_, j) => columns * (i - 1) + (j + 1)));
+  }
+
+  queries.forEach((query) => {
+    const [x1, y1, x2, y2] = query;
+    const queue = [];
+
+    // x1에서 y1+1~y2, y2에서 x1+1~x2, x2에서 y2-1~y1, y1에서 x2-1~x1
+    for (let y = y1; y < y2; y++) {
+      queue.push(arr[x1 - 1][y]);
+    }
+    for (let x = x1; x < x2; x++) {
+      queue.push(arr[x][y2 - 1]);
+    }
+    for (let y = y2 - 2; y >= y1 - 1; y--) {
+      queue.push(arr[x2 - 1][y]);
+    }
+    for (let x = x2 - 2; x >= x1 - 1; x--) {
+      queue.push(arr[x][y1 - 1]);
+    }
+
+    answer.push(Math.min(...queue)); // 최솟값 구하기
+    queue.unshift(queue.pop());
+
+    for (let y = y1; y < y2; y++) {
+      arr[x1 - 1][y] = queue.shift();
+    }
+    for (let x = x1; x < x2; x++) {
+      arr[x][y2 - 1] = queue.shift();
+    }
+    for (let y = y2 - 2; y >= y1 - 1; y--) {
+      arr[x2 - 1][y] = queue.shift();
+    }
+    for (let x = x2 - 2; x >= x1 - 1; x--) {
+      arr[x][y1 - 1] = queue.shift();
+    }
+  });
+
+  return answer;
+}
