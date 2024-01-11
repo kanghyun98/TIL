20d11a2a8fe31aeb6450c390d7eba04687ab992d
kanghyun98 <lkh981107@gmail.com>
2024-01-11 23:53:19 +0900

    update Programmers solution

--- 04. CS/알고리즘/programmers/level02/68. 숫자 블록.js
+++ 04. CS/알고리즘/programmers/level02/68. 숫자 블록.js
@@ -8,13 +8,26 @@ function solution(begin, end) {
   return arr;
 }
 
+// 약수 중 가장 큰 값 (본인 제외)
 function getNumber(num) {
   const MAX = 10000000;
 
   if (num === 1) return 0;
 
+  const list = [];
+
   for (let i = 2; i <= Math.sqrt(num); i++) {
-    if (num % i === 0 && num / i <= MAX) return num / i;
+    if (num % i === 0 && i <= MAX) {
+      list.push(i);
+
+      if (num / i <= MAX && num / i !== num) {
+        list.push(num / i);
+      }
+    }
+  }
+
+  if (list.length > 0) {
+    return Math.max(...list);
   }
 
   return 1;
